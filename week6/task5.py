82da6bf5fa013995d16841bbdcedd0631e5d2f96
KiraAMonev <kamonev@edu.hse.ru>
2024-11-16 18:16:17 +0300

    Task4 and Task5 were solved

---
+++ week6/task5.py
@@ -0,0 +1,31 @@
+"""
+leetcode.com/problem-list/sliding-window
+url: https://leetcode.com/problems/binary-subarrays-with-sum/description/
+"""
+
+
+class Solution:
+    def numSubarraysWithSum(self, nums: list[int], goal: int) -> int:
+        arr = []
+        zeros = 0
+        for c in nums:
+            if c == 0:
+                zeros += 1
+            else:
+                arr.append(zeros)
+                zeros = 0
+
+        ones = len(arr)
+        if ones < goal:
+            return 0
+        arr.append(zeros)
+        out = 0
+
+        if goal == 0:
+            for x in arr:
+                out += (x * (x + 1)) // 2
+            return out
+
+        for i in range(ones - goal + 1):
+            out += (arr[i] + 1) * (arr[i + goal] + 1)
+        return out
